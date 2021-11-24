a3c0ba0fd45c9f83aee2bfa2269362bc1ded9c45
AmarjithTK <amarjithraveendran@gmail.com>
2021-11-24 16:46:31 +0530

    sample express page and body-parser for req.body
     - simple form page with express
     - app.use('/',(req,res,next) ) this will execute for all urls that start with /
     - next() function should not be used without purpose
     - app.use(/) should be placed at bottom
     - it is req.send not app.send

--- expressjs/l1/app.js
+++ expressjs/l1/app.js
@@ -1,19 +1,44 @@
 const http = require('http')
+const bodyParser = require('body-parser')
 
+ 
 const express = require('express')
 
 const app =  express()
 
-app.use((req,res,next)=>{
-    console.log('this is function 1')
-    next()
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<h2>add product page of expressjs</h2><form action="/product" method="POST"><input type="text" name="product" id=""><button type="submit">submit</button></form>')
+    // next()
+    res.redirect('/')
+
 })
 
 
-app.use((req,res,next)=>{
-    console.log('this is function 2')
-    res.send('<h1> This is express framework </h1>')
+
+app.use('/product',(req,res,next)=>{
+    res.send('<h1>product page of express</h1>')
+    console.log(req.body)
+    // next() next func should not be called at this moment, Why ?
 })
+
+
+
+// app.use('/') will run always a url starts with / 
+
+app.use('/',(req,res,next)=>{
+    console.log('this will run always')
+    res.send('<h1>Homepage of express</h1>')
+    // next() next func should not be called at this moment, Why ?
+})
+
+
 
 
 // const server = http.createServer(app)
