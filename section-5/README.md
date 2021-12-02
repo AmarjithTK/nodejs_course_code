6e2bcf0f97dcf50e239569c9f091edb9c94e47d0
AmarjithTK <amarjithraveendran@gmail.com>
2021-12-02 09:18:58 +0530

    ejs template finished , still have basic knowledge only

---
+++ section-5/README.md
@@ -0,0 +1,8 @@
+## Templating engine pug
+
+Node js based pug templating engine sample webpage
+
+![alt text](screengrab1.png)
+![alt text](screengrab2.png)
+
+
---
+++ section-5/routes/admin.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path = require('path')
+const { options } = require('./shop')
+const router  = express.Router()
+
+
+/// this is /admin/add-product GET request
+
+
+const productData = [];
+///  this is /admin/add-product POST request
+
+router.post('/add-product',(req,res,next)=>{
+    // res.sendFile(path.join(require.main.path,'methodview','shop.html'))
+    console.log(req.body)
+
+
+    
+    productData.push({title:req.body.name})
+    console.log(productData)
+    res.redirect('/')
+
+    // next() next func should not be called at this moment, Why ?
+  // res.redirect('/')
+
+})
+
+router.get('/add-product',(req,res)=>{
+  res.render('add',{pageTitle:'Add product page',path:'add-product',activeProducts:true})
+})
+
+
+// module.exports = router
+
+module.exports.router = router
+module.exports.productData = productData
