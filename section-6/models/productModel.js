02116c56b3258454260583e8a8ba20a076baf5b4
AmarjithTK <amarjithraveendran@gmail.com>
2021-12-02 22:30:51 +0530

    refactored model code a bit , looks much cleaner and polished

--- section-6/models/productModel.js
+++ section-6/models/productModel.js
@@ -2,51 +2,57 @@ const fs = require('fs')
 const path = require('path')
 
 
+const p = path.join(require.main.path, 'data', 'product.json')
+
+const getFileData = cb => {
+
+    fs.readFile(p, (err, Filedata) => {
+        let products = [];
+
+        if (!err) {
+            products = JSON.parse(Filedata) 
+            return cb(products)// this will add existing content to products
+        }
+
+
+        cb(products)
+
+
+    })
+
+}
+
 
 module.exports = class Product {
 
 
-    constructor(t){
+    constructor(t) {
         this.title = t
     }
 
-     static getPath(){
-         return path.join(require.main.path,'data','product.json')
-     }
 
-    save(){
 
-    fs.readFile(Product.getPath(),(err,Filedata)=>{
-        let products = [];
+    save() {
 
-        if(!err){
-            products = JSON.parse(Filedata) // this will add existing content to products
-        }
 
-        products.push(this) // only new content to products
 
-        fs.writeFile(Product.getPath(),JSON.stringify(products),(err) =>{
-            console.log(err)
+        getFileData((products)=>{
+            products.push(this) 
+
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                        console.log(err)
+             })
         })
-
-    })
 
 
     }
 
 
     // I feel  like static is similar keyword to --> @classmethod in python
-    static fetchAll(cb){
-        fs.readFile(Product.getPath(),(err,filedata) =>{
+    static fetchAll(cb) {
 
-            if(err){
-                return cb([]);
-            }
+        getFileData(cb);
 
-            const products = JSON.parse(filedata)
-            return cb(products)
-
-        })
     }
 
 
